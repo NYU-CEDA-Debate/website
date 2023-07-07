95da09c6dff8b0780bcf6d2718f70e50059a6213
Shreeram Modi <shreeram@Shreerams-MacBook-Air.local>
2023-07-07 11:00:39 -0700

    add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["content/**/*.md", "layouts/**/*.html"],
+  theme: {
+    fontFamily: {
+      "sans": ['"Montserrat"'],
+      "serif": ['"Frank Ruhl Libre"']
+    },
+    extend: {
+      colors: {
+        nyuViolet: "#57068c",
+        nyuUltraViolet: "#8900e1",
+        nyuBlack: "#000000",
+        nyuDeepViolet: "#330662",
+        nyuMediumViolet1: "#702b9d",
+        nyuMediumViolet2: "#7b5aa6",
+        nyuLightViolet1: "#ab82c5",
+        nyuLightViolet2: "#eee6f3",
+      },
+      lineHeight: {
+        "nav-bar": "7.66667rem"
+      }
+    },
+    srceens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px"
+    },
+  },
+  plugins: [],
+}
