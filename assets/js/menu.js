fd0404db85c8f19b2e384faf557b6fb74ea004ba
Shreeram Modi <shreerammodi10@gmail.com>
2023-08-10 17:44:20 +0530

    update script

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,7 +1,33 @@
-const btn = document.getElementById("menu-btn");
+const mainBtn = document.getElementById("main-btn");
+const menuBtn = document.getElementById("menu-btn");
 const nav = document.getElementById("menu");
+const overlay = document.getElementById("overlay");
+const body = document.body;
 
-btn.addEventListener("click", () => {
-  btn.classList.toggle("open");
+mainBtn.addEventListener("click", () => {
+  mainBtn.classList.toggle("hidden");
   nav.classList.toggle("show");
+
+  body.classList.toggle("overflow-hidden");
+
+  overlay.classList.toggle("opacity-0");
+  overlay.classList.toggle("opacity-50");
+
+  menuBtn.classList.toggle("hidden");
+  menuBtn.classList.toggle("flex");
+});
+
+menuBtn.addEventListener("click", () => {
+  menuBtn.classList.toggle("hidden");
+  nav.classList.toggle("show");
+
+  body.classList.toggle("overflow-hidden");
+
+  overlay.classList.toggle("opacity-0");
+  overlay.classList.toggle("opacity-50");
+
+  // TODO: wait until slide animation finishes before revealing main btn
+
+  mainBtn.classList.toggle("hidden");
+  mainBtn.classList.toggle("flex");
 });
