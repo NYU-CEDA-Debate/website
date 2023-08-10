019ebfd96e4ceba6ffd30e154915fcbcc8ca0d80
Shreeram Modi <shreerammodi10@gmail.com>
2023-08-10 17:44:01 +0530

    change tailwind dir to use docs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["src/**/*.html", "src/*.html"],
+  content: ["docs/**/*.html", "docs/*.html"],
+  safelist: ["overflow-hidden", "opacity-70"],
   theme: {
     fontFamily: {
       sans: ["Montserrat"],
